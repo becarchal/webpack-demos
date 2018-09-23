42d5ae31f788685b27432d3f6ebd3fb36447fac2
杨斌 <Bin2.Yang@five-star.cn>
2018-09-23 19:33:57 +0800

    添加webpack最新版功能试例

---
+++ demoTryLastest/webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash:8].js',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 50000, // 包最小大小
+			// maxSize: 50000, // 包最大大小
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+    ]
+  },
+};
